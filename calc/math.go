2e3121d0e360fd7194a991c76e9936495d13d81f
Manoharan Gurusamy <manoharan.gurusamy@gmail.com>
2020-11-25 11:02:52 +0000

    added variable arguments to Add function

--- calc/math.go
+++ calc/math.go
@@ -1,6 +1,10 @@
 package calc
 
-// function Add - to calculate sum of two integers
-func Add(i int, j int)int  {
-	return i + j
+// Add - to calculate sum of two integers
+func Add(numbers ...int)int  {
+	sum := 0;
+	for _, num := range numbers {
+		sum = sum + num
+	}
+	return sum
 }
