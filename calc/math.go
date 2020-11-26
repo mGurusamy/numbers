ec997d3ffeba70a3a69c18f28646fbeb863fcac6
Manoharan Gurusamy <manoharan.gurusamy@gmail.com>
2020-11-26 11:27:27 +0000

    bug fixes - removed the extra bracket

--- calc/math.go
+++ calc/math.go
@@ -5,14 +5,14 @@ import (
 )
 
 // Add - to calculate sum of two integers
-func Add(numbers ...int)(error, int)  {
-	sum := 0;
-	if len(numbers) < 2){
+func Add(numbers ...int) (error, int) {
+	sum := 0
+	if len(numbers) < 2 {
 		return errors.New("Minimum two arguments required for add function"), sum
 	} else {
 		for _, num := range numbers {
 			sum = sum + num
 		}
-	}	
+	}
 	return nil, sum
 }
